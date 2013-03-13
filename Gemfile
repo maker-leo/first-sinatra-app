7ef775fef1c085a0cf83c83f379793c92aafc233
Leo Allen <leo.f.allen@gmail.com>
2013-03-13 15:57:10 +0000

    Added shotgun, tidied up and renamed main sinatra file

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+# This installs sinatra
 gem "sinatra"
 gem "sinatra-contrib"
+
+# We use this to reload our server whenever we make changes
+gem "shotgun"
--- sinatra-app.rb
+++ sinatra-app.rb
@@ -1,10 +1,10 @@
 # Remember to run the server when testing locally
-# by going ruby heroku-sinatra-app.rb
-# then go to localhost:4567 in your browser
+# by going shotgun sinatra-app.rb
+# then go to localhost:9393 in your browser
 #
 require 'bundler/setup'
 require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'sinatra/contrib'
 
 
 # for templates uncomment the line below
@@ -16,5 +16,7 @@ get '/' do
   # This will be your default route
   'change me!'
 end
+
+# Try adding some other routes
 
 # Test at <appname>.heroku.com (you'll need to create your app first!)
